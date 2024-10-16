217fe815c378c10486ff31589421539c4d2a8b77
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-10-16 11:26:59 +0800

    make CI to failed on fail tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,26 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+// GetAPIKey -
+func TestGetAPIKey(t *testing.T) {
+	t.Run("testing GetAPIKey()", func(t *testing.T) {
+		t.Fail()
+		expected := "ApiKey mockkey"
+
+		header := http.Header{}
+		header.Set("Authorization", expected)
+
+		result, err := GetAPIKey(header)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if result != "mockkey" {
+			t.Errorf("\nexpected: %v\nactual: %v", "mockkey", result)
+		}
+	})
+}
